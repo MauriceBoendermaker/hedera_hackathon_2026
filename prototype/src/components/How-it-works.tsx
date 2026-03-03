12915d40696d2994118921799731994e08e12704
Maurice <mauriceboendermaker@gmail.com>
2026-03-03 21:04:41 +0100

    Replace <br /> spacing with proper CSS in "How It Works"

--- prototype/src/components/How-it-works.tsx
+++ prototype/src/components/How-it-works.tsx
@@ -13,13 +13,9 @@ function HowItWorks() {
                         </p>
                         <ul>
                             <li>Random links are generated using a keccak256 hash of the sender address, original URL, and timestamp.</li>
-                            <br />
                             <li>Custom links require a 1 HBAR payment and are reserved by the slug you choose.</li>
-                            <br />
                             <li>Links are stored in a public smart contract and can be resolved by anyone using the short ID.</li>
-                            <br />
                             <li>The front-end at <a className="btn-link" href="https://durl.dev" target="_blank" rel="noopener noreferrer">durl.dev</a> interacts with the chain via MetaMask and ethers.js through Hedera's JSON-RPC relay.</li>
-                            <br />
                             <li>Styling is handled using Bootstrap 5 and custom SCSS.</li>
                         </ul>
                     </div>
