501c62ad8d115c1b93d12b0f0592189c10f160c3
Maurice <mauriceboendermaker@gmail.com>
2026-03-03 14:23:50 +0100

    Changed port from 3000 to 5000

--- prototype/analytics/server.cjs
+++ prototype/analytics/server.cjs
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 const LOG_FILE = path.join(__dirname, 'logs.json');
 
 app.use(bodyParser.json());
